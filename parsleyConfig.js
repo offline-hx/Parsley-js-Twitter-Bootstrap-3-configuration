41b71c3d08d92b1aa1d9c257e2df316ad14c8cd0
脱机 <350116453@qq.com>
2015-01-21 11:57:51 +0800

    添加了另一种配置方法
    添加了另一种配置方法

--- parsleyConfig.js
+++ parsleyConfig.js
@@ -1,3 +1,4 @@
+//统一配置，按需加载，不同配置的页面加载不同的配置
 window.ParsleyConfig = {
     errorClass: 'has-error',
     successClass: 'has-success',
@@ -10,3 +11,16 @@ window.ParsleyConfig = {
     errorsWrapper: '<span class="help-block">',
     errorTemplate: '<div></div>'
 };
+
+//统一配置，不同页面书写自己的配置
+window.ParsleyConfig = window.ParsleyConfig || {};
+window.ParsleyConfig.errorClass=!window.ParsleyConfig.errorClass ? 'has-error' : window.ParsleyConfig.errorClass;
+window.ParsleyConfig.successClass=!window.ParsleyConfig.successClass ? 'has-success' : window.ParsleyConfig.successClass;
+window.ParsleyConfig.classHandler = !window.ParsleyConfig.classHandler ? function (ParsleyField) {
+    return ParsleyField.$element.parents('.form-group');
+} : window.ParsleyConfig.classHandler;
+window.ParsleyConfig.errorsContainer = !window.ParsleyConfig.errorsContainer ? function (ParsleyField) {
+    return ParsleyField.$element.parents('.form-group');
+} : window.ParsleyConfig.errorsContainer;
+window.ParsleyConfig.errorsWrapper=!window.ParsleyConfig.errorsWrapper ? '<span class="help-block">' : window.ParsleyConfig.errorsWrapper;
+window.ParsleyConfig.errorTemplate=!window.ParsleyConfig.errorTemplate ? '<div></div>' : window.ParsleyConfig.errorTemplate;
